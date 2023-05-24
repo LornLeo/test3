d34a889868bfb85572a85b4ca0c01908a0f397ab
LornLeo <134342527+LornLeo@users.noreply.github.com>
2023-05-24 12:52:18 +1200

    Add files via upload

---
+++ function.py
@@ -0,0 +1,29 @@
+'''def add(num1,num2):
+    print("Number 1:",num1)
+    print("Number 2:",num2)
+    addtion=num1+num2
+    return addtion
+res = add(2,4)
+print(res)'''
+'''def add():
+    total=3
+    print(total)
+    abc=4
+    return abc
+#a=int(input("enter a value: "))
+#b=int(input("enter b value: "))
+print(add())
+def my_sum(a,b,c):
+    total=a+b+c
+    return total
+a=int(input("enter a value: "))
+b=int(input("enter b value: "))
+c=int(input("enter c value: "))
+print("Total is: ",my_sum(a,b,c))'''
+def total(totaL):
+    def totaL(a):
+        a=5
+        totaL=totaL-a
+        return totaL
+    return totaL
+print(total(2))
